9fb1e874736b7a24875f04fb2e762a1e0bd2586d
Juan Diaz <juannavegante2010@gmail.com>
2025-12-12 12:01:25 -0500

    correcciones

--- src/app/components/HomeComponents/ctaContactForm.jsx
+++ src/app/components/HomeComponents/ctaContactForm.jsx
@@ -3,17 +3,22 @@ import ContactForm from "../ContactForm";
 export default function CtaContactForm() {
   return (
     <section id="cta" className="mt-[70px]  home">
-      <div className="xl:container mx-auto lg:max-w-[1200px] px-[24px] xl:px-0 flex flex-wrap items-start">
-        <div className="lg:w-8/12 lg:pt-48 relative">
-          <h2 className="text-[40px] lg:text-[110px] font-black italic lg:pt-8 leading-[45px] lg:leading-[110px] lg:pb-12 ps-[28px] lg:ps-0 pt-[2px] ">
-            <span className="text-stroke">
-              READY TO <br className="block lg:hidden"></br>
+      <div className="lg:container mx-auto lg:max-w-[1200px] px-[24px] xl:px-0 flex flex-wrap items-start">
+        <div className="lg:w-8/12 lg:pt-[38px] relative lg:pb-[58px]">
+          <h2 className="text-[40px] lg:text-[100px] font-black italic leading-[45px] lg:leading-[110px] lg:pb-12 ps-[28px] lg:ps-0 pt-[2px] ">
+            <span className="text-stroke ps-0 lg:ps-[55px]">
+              READY TO <br></br>
             </span>{" "}
-            <span className="lg:ps-6">STAND OUT?</span>
-            <div className="block lg:hidden absolute top-0 left-0 w-full h-[200px] border-l-1 border-l-white border-t-1 border-t-white transform rotate-[-10.894deg]"></div>
+            <span className="ps-0 lg:ps-[92px]">STAND OUT?</span>
+            <div className="absolute top-0 left-0 max-w-[315px] w-full h-[200px] lg:h-[306px] border-l-1 border-l-white border-t-1 border-t-white transform rotate-[-10.894deg]"></div>
           </h2>
         </div>
-        <div className="lg:w-5/12 flex flex-wrap lg:ms-[-120px] justify-center">
+
+
+        <div className="lg:w-6/12 lg:mt-[-50px] lg:px-16 lg:pb-24">
+          <ContactForm />
+        </div>
+                <div className="lg:w-5/12 flex flex-wrap lg:ms-[-120px] justify-center">
           <div className="w-full justify-center flex lg:pe-16">
             <div className="border border-[#ffffff] -rotate-15 w-fit">
               <div className="rotate-15 flex lg:px-4 lg:py-14">
@@ -73,10 +78,6 @@ export default function CtaContactForm() {
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="lg:w-6/12 lg:mt-[-50px] lg:px-16 lg:pb-24">
-          <ContactForm />
         </div>
 
         <div className="lg:w-6/12 lg:ps-8 lg:pt-20">
